ef3afa967119d475f64107be6a831a15820edbf9
ardvidn <ardvidn93@gmail.com>
2025-05-08 15:59:50 +0700

    backend PBB finished

--- backend_PBB/src/router/retrieve.router.ts
+++ backend_PBB/src/router/retrieve.router.ts
@@ -1,8 +1,15 @@
 import { getAllDatObjekPajak } from "@/controller/getDatObjekPajak";
+import { getKodeKecamatan, getKodeKelurahan } from "@/controller/getKodeWilayah";
+import { getKodeZnt } from "@/controller/getKodeZnt";
+import { getWajibPajak } from "@/controller/getWajibPajak";
 import { Router } from "express";
 
 const retrieveRouter = Router();
 
 retrieveRouter.get("/datobjekpajak", getAllDatObjekPajak as any);
+retrieveRouter.get("/kodekecamatan", getKodeKecamatan as any);
+retrieveRouter.get("/kodekelurahan", getKodeKelurahan as any);
+retrieveRouter.get("/kodeznt", getKodeZnt as any);
+retrieveRouter.get("/wajibpajak", getWajibPajak as any);
 
 export default retrieveRouter;
