b0733829ebf5bd9f1538e9bff2ab8c8d87ce4abb
ardvidn <ardvidn93@gmail.com>
2025-05-09 15:26:59 +0700

    fixing cookies yang hilang setelah login bagian auth: menghilangkan pengambilan cookies di /pendataan

--- frontend/src/app/pendataan/page.tsx
+++ frontend/src/app/pendataan/page.tsx
@@ -1,31 +1,30 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import { logged } from "../../utils/interface";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+// import { logged } from "../../utils/interface";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
+// import React, { useEffect } from "react";
 
 const pendataan = () => {
-  const router = useRouter();
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const res = await axios.get<logged>(
-          `${process.env.NEXT_PUBLIC_PENDATAAN_API_URL}/api/auth/me`,
-          { withCredentials: true, timeout: 5000 } // Tambah timeout
-        );
+  // const router = useRouter();
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     try {
+  //       const res = await axios.get<logged>(
+  //         `${process.env.NEXT_PUBLIC_PENDATAAN_API_URL}/api/auth/me`,
+  //         { withCredentials: true, timeout: 5000 } // Tambah timeout
+  //       );
 
-        if (!res.data.loggedIn) {
-          router.push("/login");
-        }
-      } catch (error) {
-        console.error("Auth check failed:", error);
-        router.push("/login"); // Pastikan redirect saat error
-      }
-    };
+  //       if (!res.data.loggedIn) {
+  //         router.push("/login");
+  //       }
+  //     } catch (error) {
+  //       console.error("Auth check failed:", error);
+  //       router.push("/login"); // Pastikan redirect saat error
+  //     }
+  //   };
 
-    checkAuth();
-  }, [router]);
+  //   checkAuth();
+  // }, [router]);
   return (
     <>
       <div>asdad</div>
